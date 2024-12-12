7c7552fe3c4dc956d5294dc080e9aa7df762465a
Axeltaylor36 <alejandrosastre36@gmail.com>
2024-12-12 19:22:28 +0100

    NPC y Jugador Meshes

---
+++ Assets/Scripts/playerVisual.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class playerVisual : MonoBehaviour
+{
+
+    private Animator anim;
+
+    [SerializeField] private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent <Animator>();   
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //agent.velocity.magnitude --> Velocidad actual...
+        //agent.speed --> Máxima velocidad que tengo configurada
+
+        anim.SetFloat("velocity", agent.velocity.magnitude / agent.speed);
+
+    }
+}
