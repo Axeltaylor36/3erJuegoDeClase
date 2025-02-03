d909a9b1ece380ee74682f34641cb36cf8b2c3f2
DESKTOP-GV16N45\Alumno <alejandrosastre36@gmail.com>
2025-02-03 21:17:35 +0100

    Paredes invisibles y parede spor mapa

---
+++ Assets/Scripts/WallsDetector.cs
@@ -0,0 +1,34 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallsDetector : MonoBehaviour
+{
+
+    [SerializeField]
+    private float tiempoDifuminado;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Obstaculo"))
+        {
+
+            Material materialPared = other.GetComponent<MeshRenderer>().material;
+            Color transparente = new Color(materialPared.color.r, materialPared.color.g, materialPared.color.b, 0f);
+            materialPared.DOColor(transparente, 1f);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Obstaculo"))
+        {
+
+            Material materialPared = other.GetComponent<MeshRenderer>().material;
+            Color opaco = new Color(materialPared.color.r, materialPared.color.g, materialPared.color.b, 1f);
+            materialPared.DOColor(opaco, 1f);
+
+        }
+    }
+}
